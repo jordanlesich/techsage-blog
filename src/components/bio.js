9fd1cb7537ef3bb6f9a3de5cc6737cd65d430f77
jordan.lesich <jordan.lesich@gmail.com>
2020-03-20 11:53:48 -0400

    added image widget

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,11 +34,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong>, a web developer living in Toronto. 
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Follow on Twitter
               </a>
             </p>
           </Container>
