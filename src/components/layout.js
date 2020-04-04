7c793d3d8e4932bd5a8cc63902b18ad8c0ece202
jordan.lesich <jordan.lesich@gmail.com>
2020-04-04 16:04:05 -0400

    removed google fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Header from './header'
 import Footer from './footer'
-import Helmet from "react-helmet"
 import styled, {createGlobalStyle} from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
@@ -13,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
   body {
-    font-family: 'Roboto', sans-serif, -apple-system,  BlinkMacSystemFont, 'Segoe UI',  Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system,  BlinkMacSystemFont, 'Segoe UI',  Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 18px;
     color: #262626;
     text-rendering: optimizeLegibility;
@@ -44,9 +43,6 @@ const Layout = props => {
     return (
       
       <>
-        <Helmet>
-              <link href="https://fonts.googleapis.com/css?family=Roboto:300,400, 500,700&display=swap" rel="stylesheet" />
-        </Helmet>
         <GlobalStyle />
           <Header location={location}/>
           <MainLayout>
